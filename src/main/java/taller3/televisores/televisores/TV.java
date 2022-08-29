e6ece6b9fb2fb12768a8c374c97752bcd2eee937
Yeferson05 <yaguilar@unal.edu.co>
2022-08-29 16:47:17 -0500

    Update TV.java

--- src/main/java/taller3/televisores/televisores/TV.java
+++ src/main/java/taller3/televisores/televisores/TV.java
@@ -43,7 +43,7 @@ public class TV {
 		return volumen;
 	}
 	public void setCanal (int canal) {
-		if (getEstado() == true && getCanal()<= 121) {
+		if (estado == true && getCanal()< 121) {
 			this.canal = canal;
 		}
 	}
